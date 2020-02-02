e403f382c5a8f067edfafd7ad4477076894885d0
nemesmatyas <nemesmatyas@gmail.com>
2020-02-02 10:55:53 +0100

    Added ShopPage, CollectionItem and CollectionPreview component

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import { Switch, Route } from 'react-router-dom';
 
 import Homepage from './pages/homepage/homepage.component';
+import Shoppage from './pages/shoppage/shoppage.component';
 
 function App() {
   return (
     <div className="App">
       <Switch>
         <Route exact path="/" component={Homepage} />
+        <Route path="/shop" component={Shoppage} />
       </Switch>
     </div>
   );
