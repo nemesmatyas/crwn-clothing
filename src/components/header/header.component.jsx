48b0de193b337be07cfe82592f29d22d2f726b11
nemesmatyas <nemesmatyas@gmail.com>
2020-02-08 14:34:10 +0100

    Added Cart

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -6,8 +6,10 @@ import { connect } from 'react-redux';
 import './header.styles.scss';
 
 import { ReactComponent as Logo } from '../../assets/crown.svg';
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 
-const Header = ({ currentUser }) => {
+const Header = ({ currentUser, hidden }) => {
     return(
         <div className="header">
             <Link className="logo-container" to="/">
@@ -20,14 +22,18 @@ const Header = ({ currentUser }) => {
                 currentUser ? <div className="option" onClick={() => auth.signOut()}>SIGN OUT</div>
                  : <Link className="option" to="/signin">SIGN IN</Link>
                 }
+                <CartIcon />
             </div>
-            
+            {
+                hidden ? null : <CartDropdown />
+            }
         </div>
     )
 }
 
-const mapStateToProps = state => ({
-    currentUser: state.user.currentUser
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden }}) => ({
+    currentUser,
+    hidden
 })
 
 
